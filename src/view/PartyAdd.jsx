a36c5ec0299c2cec4ed1ccd263578283a68a2565
mxz96102 <mxz96102@qq.com>
2017-04-08 14:06:40 +0800

    4.7

--- src/view/PartyAdd.jsx
+++ src/view/PartyAdd.jsx
@@ -11,14 +11,16 @@ export default class SchoolAdd extends Component {
       opt:"",
       colleage:{},
       colleageid:"",
-      content:{}
+      content:{},
+      option:[],
+      id:""
     }
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
 
-    axios.get("/party/createParty?name="+getByName("name"))
+    axios.get("/party/createParty?name="+getByName("name")+"&uniid="+this.state.id)
       .then((res)=>{
         if(res.data.msg === "SUCCESS"){
           alert("提交成功");
@@ -30,13 +32,31 @@ export default class SchoolAdd extends Component {
       })
   }
 
-  handleChange = (value) => {
+  handleChange(value){
     this.setState({
-      opt : value
+      id: value.split("-")[0]
     })
+
   }
 
   componentDidMount(){
+    let __this = this;
+
+    axios.get("/uni/allUniversity")
+      .then((res)=>{
+        let i,data = res.data.result;
+
+        if(res.data.msg === "SUCCESS"){
+          __this.state.option= [];
+          for(i=0;i<data.length;i++){
+            __this.state.option.push(<Select.Option key={data[i].id}>{data[i].id+"-"+data[i].uniname}</Select.Option>)
+          }
+
+          __this.setState({
+            option : __this.state.option
+          })
+        }
+      });
 
   }
 
@@ -55,6 +75,11 @@ export default class SchoolAdd extends Component {
         <Form onSubmit={this.handleSubmit}>
           <Form.Item label="圈子名称">
             <Input name="name" type="text" autosize required/>
+          </Form.Item>
+          <Form.Item label="学校">
+            <Select style={{minWidth:"10rem"}} onSelect={this.handleChange.bind(this)} >
+              {this.state.option}
+            </Select>
           </Form.Item>
           <Button type="primary" htmlType="submit" className="login-form-button">
             提交
